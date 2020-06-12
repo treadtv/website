fc6209fdaa44640cdcfd60ecc06fe0e499037d8a
Anshul <anshulravikumar@gmail.com>
2020-06-12 10:35:27 +0530

    correct website

--- app.js
+++ app.js
@@ -26,97 +26,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join('public')));
-app.use(expressSession({
-  secret: "dffhdgnfmjhrgfgmghmjmdrdeyjhmhmjgmfnghmhjfgfnghmghmhjmjhmjhjhnvvb"
-}));
-app.use(passport.initialize());
-app.use(passport.session()); //to get some data and not query db every login time
-
-passport.use(new localStrategy(
-  {usernameField : "email",
-passwordField : "password"},
-  function(email, password, next) {
-    User.findOne({ email: email }, function (err, user) {
-      if (err)  return next(err); 
-      if (!user || !bcrypt.compareSync(password,user.passwordHash))  return next({message:'wrong email/password'});    
-      
-      next(null,user);  
-    }
-    );
-  }
-));
-
-passport.use('signup-local',new localStrategy(
-  {usernameField : "email",
-passwordField : "password"},
-  function(email, password, next) {
-    User.findOne({
-      email:email
-    }, function(err,user){
-      if(err) return next(err);
-      if(user) return next({message:'User already exists'});
-      let newUser  = new User ({
-        email : email,
-        passwordHash : bcrypt.hashSync(password,10)
-    });
-    newUser.save(function(err){
-     next(err,newUser);
-    });
-    }
-    );
-  }
-));
-
-// to reduce size of user
-passport.serializeUser(function(user,next){
-next(null,user._id); 
-});
- 
-passport.deserializeUser(function(id,next){
-  User.findById(id,function(err,user){
-    next(err,user);
-  });
-});
 
 app.get('/',function(req, res, next) {
-    res.render('index',{title:'KWAN tutorial'});
+    res.render('index');
 });
-app.get('/main',function(req, res, next) {
-  res.render('main');
-});
-
-app.get('/builder',function(req, res, next) {
-  res.render('builder');
-});
-app.get('/workouts/:id',function(req, res, next) {
-res.render('workout');
- 
-
-});
-app.get('/walkthrough',function(req, res, next) {
-  req.session.sawWalkthrough = 1;
-  res.end();
-});
-app.get('/complicated',function(req, res, next) {
-  console.log( req.session.sawWalkthrough);
+app.get('/success',function(req, res, next) {
+  res.render('success');
 });
 
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login-page' }),
-  function(req, res) {
-    res.redirect('/main');
-  });
-
-
-app.get('/login-page',function(req, res, next) {
-  res.render('login-page');
-});
-//for more sensitive data use post
-app.post('/signup', 
-  passport.authenticate('signup-local', { failureRedirect: '/login-page' }),
-  function(req, res) {
-    res.redirect('/main');
-  });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -134,3 +51,21 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+//Heroku Build Process - Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+
+  app.use(express.static('client/build'));
+
+  app.get('*', (request, response) => {
+      response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+
+}
+
+//Define Port and Start server
+const port = process.env.PORT || 5000;
+app.listen(port, function (req, res) {
+  console.log(`Server running at ${port}`);
+});
